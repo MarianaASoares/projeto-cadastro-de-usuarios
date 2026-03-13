90024d49c776dfad1b295fd23dfd1d2a05368f09
MarianaASoares <mariluusoares@gmail.com>
2026-03-13 15:09:34 -0300

    Atualiza API para Render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://backend-cadastro-usuarios-w6sk.onrender.com'
 })
 
 export default api;
